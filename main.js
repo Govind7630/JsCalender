dd172c6887085b4a261b4830a52beb90982c1691
Govind7630 <123011059+Govind7630@users.noreply.github.com>
2025-06-14 11:27:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
 class BookingCalendar extends HTMLElement {
   connectedCallback() {
-    // Inject HTML structure
     this.innerHTML = `<div id="calendar"></div>`;
 
-    // Add FullCalendar CSS
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/index.global.min.css';
     document.head.appendChild(link);
 
-    // Add FullCalendar JS
     const script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/index.global.min.js';
     script.onload = () => {
-      const calendarEl = this.querySelector('#calendar');
-      const calendar = new FullCalendar.Calendar(calendarEl, {
+      const calendar = new FullCalendar.Calendar(this.querySelector('#calendar'), {
         initialView: 'dayGridMonth',
         selectable: true,
-        dateClick: function(info) {
-          alert('Clicked on: ' + info.dateStr);
-        }
+        dateClick(info) {
+          alert("Clicked on: " + info.dateStr);
+        },
       });
+
       calendar.render();
     };
     document.body.appendChild(script);

5d248b162a3fc4a286fcda9da7b0e1afc2c2664f
Govind7630 <123011059+Govind7630@users.noreply.github.com>
2025-06-14 18:37:14 +0530

    events update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 class BookingCalendar extends HTMLElement {
   connectedCallback() {
-    console.log("Custom element loaded");
     this.innerHTML = `<div id="calendar"></div>`;
 
     const link = document.createElement('link');
@@ -14,9 +13,50 @@ class BookingCalendar extends HTMLElement {
       const calendar = new FullCalendar.Calendar(this.querySelector('#calendar'), {
         initialView: 'dayGridMonth',
         selectable: true,
-        dateClick(info) {
-          alert("Clicked on: " + info.dateStr);
-        },
+        events: async function(fetchInfo, successCallback, failureCallback) {
+  try {
+    const res = await fetch(
+      'http://localhost:8080/o/c/bookings', // Replace with your object name
+      {
+        headers: {
+          'Authorization': 'Basic ' + btoa('test@liferay.com:test'), // or use OAuth/token
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    const data = await res.json();
+    const events = data.items.map(item => ({
+      title: item.title,
+      start: item.startDateTime,
+      end: item.endDateTime
+    }));
+    successCallback(events);
+  } catch (err) {
+    failureCallback(err);
+  }
+},
+        dateClick: function(info) {
+  const title = prompt('Enter booking title:');
+  if (title) {
+    fetch('http://localhost:8080/o/c/bookings', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + btoa('test@liferay.com:test'),
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: title,
+        startDateTime: info.dateStr,
+        endDateTime: info.dateStr // or add +1hr logic
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      calendar.refetchEvents(); // Reload events
+      alert('Booking added!');
+    });
+  }
+}
       });
 
       calendar.render();
